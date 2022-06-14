19117adbfab57ef4ecdef67b3f8a9e6a0e0369ef
munal0803 <75110825+munal0803@users.noreply.github.com>
2022-06-14 22:34:14 +0530

    Final Update

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_firebase/auth/authscreen.dart';
+import 'dart:ui';
+import 'package:todo_firebase/screens/home.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.onAuthStateChanged,
+        builder: (context, usersnapshot) {
+          if (usersnapshot.hasData) {
+            return Home();
+          } else {
+            return AuthScreen();
+          }
+        },
+      ),
+      debugShowCheckedModeBanner: false,
+      theme:
+          ThemeData(brightness: Brightness.light, primaryColor: Colors.redAccent),
+    );
+  }
+}
